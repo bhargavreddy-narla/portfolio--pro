17fd4e086e8dc93f7e1fa9267c519de5b3048079
bhargav-reddy-narla <85004210+bhargavreddy-narla@users.noreply.github.com>
2022-01-09 13:48:05 +0530

    updated

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -37,7 +37,7 @@ const Home = () => {
               className="home--H1"
             >
               {/* I'm designer haris f. watson */}
-              Bhargav Reddy Narla
+              Bhargav Reddy Narla Bhargav
             </motion.h1>
             <motion.p
               animate={{ y: -50 }}
